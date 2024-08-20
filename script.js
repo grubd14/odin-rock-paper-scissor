1ffc22eb114f2ba6bae35f2daa0f127aabb34c15
grubd14 <83056016+grubd14@users.noreply.github.com>
2024-08-20 21:50:41 +0545

    function to play a round

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 //function to get random value from 0 to 2
 //in int with long decimal value
+//
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
   let randomValue = Math.floor(Math.random() * 3);
@@ -8,14 +11,36 @@ function getComputerChoice() {
   } else if (randomValue === 1 ) {
     return "paper";
   } else {
-    return "scissors";
+    return "scissor";
   }
 }
 
 //function to get human choice 
 function humanChoice() {
   let userInput = prompt("Your choice!: ");
-  console.log(userInput);
+  return userInput.toLowerCase();
 }
 
-console.log(humanChoice());
+
+
+//function to play the game 
+function playRound(humanSelection, computerSelection) {
+  if (humanSelection === "rock" && computerSelection === "scissor") {
+    return "You win!";
+  } else if(humanSelection === "scissor" && computerSelection === "paper") {
+    return "You win!"; 
+  } else if ( humanSelection === "paper" && computerSelection === "rock") {
+    return "You win!";
+  } else if (humanSelection === computerSelection) {
+    return "tie" ;
+  } else {
+    return "You lose!"
+  }
+}
+
+const humanSelection = humanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
+console.log(humanSelection);
+console.log(computerSelection);
